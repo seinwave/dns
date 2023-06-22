72d04f1ab2fe0fe01452ae8984abb130bd079a78
Matt Seidholz <mseidholz@gmail.com>
2023-06-22 07:57:02 -0400

    add failing test for parse_header

--- response/test.rb
+++ response/test.rb
@@ -7,8 +7,8 @@ describe Response do
         @response = Response.new
         @raw_response = "\x00\x01\x81\x80\x00\x01\x00\x01\x00\x00\x00\x00\x03www\aexample\x03com\x00\x00\x01\x00\x01\xc0\x0c\x00\x01\x00\x01\x00\x00\x00\x00\x00\x04\x00\x04\x7f\x00\x00\x01"
     end
-    
-    it "parse a header, and return a new DNSRecord" do
+
+    it "parses a header, and returns a new DNSRecord" do
         @buffer = StringIO.new(@raw_response)
         expect(@response.parse_header(@buffer)).to eq(DNSRecord.new(1, 1, 1, 0, 0))
     end
